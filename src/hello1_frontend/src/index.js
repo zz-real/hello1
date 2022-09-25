438caddf1bb41aca9b0e23627ff3121304342bfd
573727067@qq.com <zuodawei02126413>
2022-09-25 15:46:22 +0800

    Initial commit.

---
+++ src/hello1_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { hello1_backend } from "../../declarations/hello1_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await hello1_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
