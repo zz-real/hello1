3fc1256fb187959ad253a2346ce4dcfa9a6cae98
573727067@qq.com <zuodawei02126413>
2022-09-25 20:00:54 +0800

    第五课

--- src/hello1_backend/main.mo
+++ src/hello1_backend/main.mo
@@ -1,5 +1,87 @@
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+
 actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+
+    public type Message = {
+        text :Text;
+        author : Text;
+        time :Time.Time;
+    };
+
+    stable var followed :List.List<Principal> = List.nil();
+
+    stable var messages :List.List<Message> = List.nil();
+
+    private stable var auth: Text = "zz";
+
+    public type Microblog = actor {
+        follow: shared(Principal)  -> async (); //添加关注对象
+        follows: shared query ()  -> async [Principal]; //返回关注对象列表
+        reset_follow: shared() -> async ();
+        post: shared (Text,Text)  -> async (); //发布新消息
+        posts: shared query (Time.Time)  -> async [Message]; //返回所有发布的消息
+        timeline: shared (Time.Time)  -> async [Message]; //返回所有关注对象发布的消息
+        set_name: shared (Text) -> async ();
+        get_name: shared () -> async ?Text;
+    };
+
+    public shared func set_name(t: Text) : async () {
+        auth := t;
+    };
+
+    public shared func get_name() : async ?Text {
+        ?auth
+    };
+
+
+    public shared func follow (id :Principal) :async(){
+        followed := List.push(id, followed);
+    };
+
+    public shared func follows () :async [Principal]{
+        List.toArray(followed);
+    };
+
+    public shared func reset_follow() : async () {
+        followed := List.nil();
+    };
+
+    public shared (caller) func post (otp: Text, text :Text) :async (){
+        assert(otp == "123456");
+
+        var msg : Message = {
+            text = text;
+            time = Time.now();
+            author = auth;
+        };
+
+        messages := List.push(msg, messages);
+    };
+    
+    public shared query func posts(since : Time.Time) :async [Message]{
+        var since_message : List.List<Message> = List.nil();
+            for (msg in Iter.fromList(messages)) {
+                if (msg.time >= since) {
+                    since_message := List.push(msg, since_message);
+                };
+            };
+        return List.toArray(since_message);
+    };
+
+    public shared func timeline(since : Time.Time) : async [Message]{
+        var all :List.List<Message> = List.nil();
+
+        for (id in Iter.fromList(followed)){
+            let canister :Microblog = actor(Principal.toText(id));
+            let msgs = await canister.posts(since);
+            for(msg in Iter.fromArray(msgs)){
+                all := List.push(msg, all);
+            };
+        };
+
+        List.toArray(all);
+    };
 };
--- src/hello1_frontend/src/index.js
+++ src/hello1_frontend/src/index.js
@@ -1,19 +1,86 @@
 import { hello1_backend } from "../../declarations/hello1_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+async function post() {
+  let post_button = document.getElementById("post");
+  post_button.disabled = true;
+  let textarea = document.getElementById("message");
+  let otp = document.getElementById("otp").value;
+  let text = textarea.value;
+  try {
+    await hello1_backend.post(otp, text);
+  } catch (e) {
+    console.log(e);
+    document.getElementById("error").innerText = "Post failed";
+  }
+  post_button.disabled = false;
+}
 
-  const name = document.getElementById("name").value.toString();
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(Number(UNIX_timestamp / 1000000n));
+  const humanDateFormat = a.toLocaleString();
+  return humanDateFormat;
+}
 
-  button.setAttribute("disabled", true);
+var num_posts = 0;
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello1_backend.greet(name);
+async function load_posts() {
+  let posts_section = document.getElementById("posts");
+  let posts = await hello1_backend.posts(0);
+  if (num_posts === posts.length) return;
+  posts_section.replaceChildren([]);
+  num_posts = posts.length;
+  for (var i = 0; i < posts.length; i++) {
+    let post_metainfo = document.createElement("p");
+    post_metainfo.innerText = posts[i].author + " post in " + timeConverter(posts[i].time);
+    let post_msg = document.createElement("p");
+    post_msg.innerText = posts[i].text;
 
-  button.removeAttribute("disabled");
+    posts_section.appendChild(post_metainfo);
+    posts_section.appendChild(post_msg);
+  }
+}
 
-  document.getElementById("greeting").innerText = greeting;
+var num_follows = 0;
 
-  return false;
-});
+async function load_follows() {
+  let follows_section = document.getElementById("follows");
+  let follows = await hello1_backend.follows();
+  if (num_follows === follows.length) return;
+  follows_section.replaceChildren([]);
+  num_follows = follows.length;
+  for (var i = 0; i < follows.length; i++) {
+    let post = document.createElement("p");
+    post.innerText = follows[i];
+    follows_section.appendChild(post);
+  }
+}
+
+var num_timeline = 0;
+
+async function load_timeline() {
+  let timeline_section = document.getElementById("timeline");
+  let timeline = await hello1_backend.timeline(0);
+  if (num_timeline === timeline.length) return;
+  timeline_section.replaceChildren([]);
+  num_timeline = timeline.length;
+  for (var i = 0; i < timeline.length; i++) {
+    let timeline_metainfo = document.createElement("p");
+    timeline_metainfo.innerText = timeline[i].author + " post in " + timeConverter(timeline[i].time);
+    let timeline_msg = document.createElement("p");
+    timeline_msg.innerText = timeline[i].text;
+
+    timeline_section.appendChild(timeline_metainfo);
+    timeline_section.appendChild(timeline_msg);
+  }
+}
+
+function load() {
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  load_posts();
+  load_follows();
+  load_timeline();
+  setInterval(load_posts, 3000);
+}
+
+window.onload = load;
